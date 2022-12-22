96534ac143f843b069e4ebb49892444bf4272421
alpergltsry <alperc53@outlook.de>
2022-12-22 15:59:54 +0100

    Hauptmenü erstellt

--- app/src/main/java/com/example/sudokumobsys/Hauptmenu.kt
+++ app/src/main/java/com/example/sudokumobsys/Hauptmenu.kt
@@ -2,11 +2,11 @@ package com.example.sudokumobsys
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Hauptmenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        }
+        setContentView(R.layout.activity_hauptmenu)
+
     }
+}
