639302449828c2a8b63a1c00a039b25f07908518
Kuchenkrawatte <kalowskijr@gmail.com>
2022-12-22 21:38:17 +0100

    Launching Activity adjusted to MainMenu.kt. Added two buttons to switch between the MainMenu.kt and MainActivity.kt

--- app/src/main/java/com/example/sudokumobsys/MainMenu.kt
+++ app/src/main/java/com/example/sudokumobsys/MainMenu.kt
@@ -5,18 +5,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-
-class MainActivity : AppCompatActivity() {
+class Hauptmenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
 
-        val playButton = findViewById<Button>(R.id.homeButton)
+        val playButton = findViewById<Button>(R.id.PlayButton)
         playButton.setOnClickListener {
-            val Intent = Intent(this, Hauptmenu::class.java)
+            val Intent = Intent(this, MainActivity::class.java)
             startActivity(Intent)
         }
 
-
     }
-    }
+}
